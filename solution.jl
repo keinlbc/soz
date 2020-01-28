d6d5a6fe2f58577b091fec7c44849094f7045de1
petreze <peshyyy@abv.bg>
2020-01-28 17:13:00 +0200

    first try

--- solution.jl
+++ solution.jl
@@ -4,7 +4,7 @@ using Random
 
 horizon = 7
 
-people = ["Пешо","Гошо","Иван","Мария","Петруния","Гергинка"]
+people = ["Пешо", "Гошо", "Иван", "Мария", "Петруния", "Гергинка"]
 
 
 rng = MersenneTwister(1234)
@@ -14,35 +14,44 @@ peopleindexes = collect(permutations(1:length(people), 3))
 
 function fitness(n::AbstractVector)
 
-
-	sum = 0
+    sum = 0
     i1 = [x[1] for x in n[1:horizon]]
     i2 = [x[2] for x in n[1:horizon]]
     i3 = [x[3] for x in n[1:horizon]]
 
-    d=Dict([(i,count(x->x==i,vcat(i1,i2, i3))) for i in (1:6)]) # count occurances
+    d = Dict([(i,count(x->x == i,vcat(i1,i2, i3))) for i in (1:6)]) # count occurances
+
     for (k,v) in d
-    	
-    	sum += 2^v
+        sum += 2^v
     end
 
-    # sum += penalties()*2^horizon
+    sum += penalty(n)*2^horizon
+
     #println(append!(append!(i1,i2), i3), d, sum)
     println(sum)
 
     return sum
-    
+
     # exit()
 
 end
 
 function initpop(n::Int)
-	return shuffle!(rng, peopleindexes)[n]
+    return shuffle!(rng, peopleindexes)[n]
 end
 
+function penalty(vector::AbstractVector)
 
+    for i in 2:size(vector)-1 #to make sure we don't go out of boundaries
+        threeConsecutiveDays = vcat(vector[i-1], vector[i], vector[i+1])
 
+        for j in 1:9 # 3 days, each day has 3 shifts
+           if (count(occurences -> occurences == threeConsecutiveDays[j], threeConsecutiveDays) >= 3) true end
+        end
+    end
 
+    return false
+end
 
 
 best, invbestfit, generations, tolerance, history = Evolutionary.ga(
@@ -71,5 +80,5 @@ print(invbestfit)
 println("\nпърва - втора - нощна")
 for i in best[1:horizon]
 
-	println(people[i[1]], " - " ,people[i[2]], " - ", people[i[3]])
+    println(people[i[1]], " - " ,people[i[2]], " - ", people[i[3]])
 end
